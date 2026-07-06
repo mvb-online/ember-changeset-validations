9e00f48d8b93cafc1637a9f25eabc68e1609cf82
Melanie Schwenk <m.schwenk@mvb-online.de>
2026-07-06 14:59:05 +0200

    update used description for buildMessage rawOutput

--- test-app-raw-output/tests/unit/utils/validation-errors-test.js
+++ test-app-raw-output/tests/unit/utils/validation-errors-test.js
@@ -49,5 +49,31 @@ module('Unit | Utility | validation errors', function () {
       assert.strictEqual(type, 'date', 'the type of the error is returned');
       assert.strictEqual(value, d, 'the passed value is returned');
     });
+
+    test('#buildMessage can return a provided description in raw data structure', function (assert) {
+      let result = buildMessage('firstName', {
+        type: 'present',
+        value: 'testValue',
+        context: { foo: 'foo', description: 'Name' },
+      });
+
+      assert.notStrictEqual(
+        typeof result,
+        'string',
+        'the return value is an object',
+      );
+
+      let {
+        message,
+        type,
+        value,
+        context: { description },
+      } = result;
+
+      assert.ok(message, "{description} can't be blank");
+      assert.strictEqual(description, 'Name', 'description is returned');
+      assert.strictEqual(type, 'present', 'the type of the error is returned');
+      assert.strictEqual(value, 'testValue', 'the passed value is returned');
+    });
   });
 });
